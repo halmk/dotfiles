d80abc6a6be81e94aa3d152d227ed96498420ab2
halmk <sakurai.apple1@gmail.com>
2024-07-28 16:33:54 +0900

    add setup shell for alacritty and zellij

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+# setup dotfiles
 dotfiles=(zsh/.zshrc tmux/.tmux.conf)
 
 for file in "${dotfiles[@]}"; do
         ln -svf $PWD/$file ~/
+done
+
+# setup app config dirs
+apps=(alacritty zellij)
+
+if [ ! -d ~/.config ]; then
+  mkdir ~/.config
+fi
+
+for app in "${apps[@]}"; do
+  TARGET_DIR=~/.config/$app
+
+  if [ -d $TARGET_DIR ]; then
+    mv $TARGET_DIR $TARGET_DIR.backup
+  fi
+
+  ln -s $PWD/$app $TARGET_DIR
 done
